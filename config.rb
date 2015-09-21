b31d493dffee5750421910456f1eb312c8e9d99c
Falko Joseph <falko@typework.com>
2015-09-21 14:32:46 +0200

    Bower setup

--- config.rb
+++ config.rb
@@ -3,6 +3,11 @@ set :js_dir, "javascripts"
 set :images_dir, "images"
 set :relative_links, true
 
+sprockets.append_path File.join root, "vendor/assets/bower"
+sprockets.import_asset "jquery/dist/jquery.js"
+sprockets.import_asset "bootstrap/dist/css/bootstrap.css"
+sprockets.import_asset "jquery/dist/jquery.js"
+
 configure :build do
   activate :minify_css
   activate :minify_javascript
