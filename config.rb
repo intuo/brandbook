b046f154a9640f749720c2889e0b57f800b43115
Falko Joseph <falko@typework.com>
2015-09-22 01:34:03 +0200

    Wohoo, fixed relative assets

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 set :css_dir, "stylesheets"
 set :js_dir, "javascripts"
 set :images_dir, "images"
-set :relative_links, true
+set :relative_links, false
 
 sprockets.append_path File.join root, "vendor/assets/bower"
 sprockets.import_asset "jquery/dist/jquery.js"
@@ -19,6 +19,5 @@ activate :directory_indexes
 configure :build do
   activate :minify_css
   activate :minify_javascript
-  activate :relative_assets
-  set :http_prefix, "//intuo.github.io/brandbook/"
+  set :http_prefix, "/brandbook/"
 end
