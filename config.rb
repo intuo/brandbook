cbbf611f7094f0c87793f75918f0b4a8ce5eb85c
Falko Joseph <falko@typework.com>
2015-09-21 15:31:41 +0200

    Directory structure + partials

--- config.rb
+++ config.rb
@@ -6,9 +6,13 @@ set :relative_links, true
 sprockets.append_path File.join root, "vendor/assets/bower"
 sprockets.import_asset "jquery/dist/jquery.js"
 
+page "/docs/*", layout: "docs"
+
 activate :autoprefixer do |config|
   config.browsers = ["last 2 versions", "Explorer >= 9"]
 end
+
+activate :directory_indexes
 
 configure :build do
   activate :minify_css
